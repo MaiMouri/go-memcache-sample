999942b270b2ee05fcf54f3bc3445640c9c5c9d8
Mai Mouri <sfyitan@gmail.com>
2023-01-15 21:51:30 +0900

    rainycape

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20221031212613-62deef7fc822 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/rainycape/memcache v0.0.0-20150622160815-1031fa0ce2f2 // indirect
 )
--- mem_test.go
+++ mem_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/bradfitz/gomemcache/memcache"
-	memcache2 "github.com/rainycape/memcache"
+	goCache "github.com/patrickmn/go-cache"
+	rainycape "github.com/rainycape/memcache"
 )
 
+// gomemcache
 func BenchmarkMemLib(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
@@ -17,12 +20,25 @@ func BenchmarkMemLib(b *testing.B) {
 	}
 }
 
+// rainycape
 func BenchmarkMemLib2(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
-	mc, _ := memcache2.New("127.0.0.1:11211")
+	mc, _ := rainycape.New("127.0.0.1:11211")
 	for i := 0; i < b.N; i++ {
-		mc.Set(&memcache2.Item{Key: "test_key2", Value: []byte("test value2")})
+		mc.Set(&rainycape.Item{Key: "test_key2", Value: []byte("test value2")})
 		mc.Get("test_key2")
+	}
+}
+
+// go-cache
+func BenchmarkMemLib3(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	mc := goCache.New(5*time.Minute, 10*time.Minute)
+
+	for i := 0; i < b.N; i++ {
+		mc.Set("test_key3", "test value3", goCache.DefaultExpiration)
+		mc.Get("test_key3")
 	}
 }
